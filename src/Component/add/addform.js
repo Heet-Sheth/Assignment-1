7799377cf1df743baf1f4b05e3a91151b2d053a5
Heet-Sheth <heetsheth0000@gmail.com>
2022-02-10 15:43:06 +0530

    Prettier

--- src/Component/add/addform.js
+++ src/Component/add/addform.js
@@ -1,75 +1,156 @@
-import axios from 'axios';
-import { React, useState } from 'react';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
-import ReadForm from '../read/readform';
+import axios from "axios";
+import { React, useState } from "react";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import ReadForm from "../read/readform";
 import "./addform.css";
 
 export default function AddForm() {
-    const [userData, setData] = useState({
-        fnm: "",
-        lnm: "",
-        age: "",
-        gen: "",
-        hobby: {
-            Music: false,
-            Cricket: false,
-            Football: false,
-            Movies: false,
-            Games: false,
-        }
+  const [userData, setData] = useState({
+    fnm: "",
+    lnm: "",
+    age: "",
+    gen: "",
+    hobby: {
+      Music: false,
+      Cricket: false,
+      Football: false,
+      Movies: false,
+      Games: false,
+    },
+  });
+  const handlechange = (e) => {
+    let { name, value } = e.target;
+    if (name === "hobby") {
+      const hobby = { ...userData.hobby };
+      hobby[value] = e.target.checked;
+      setData((prev) => ({ ...prev, hobby }));
+    } else setData((prev) => ({ ...prev, [name]: value }));
+  };
+  console.log(userData);
+  const handleUpload = (e) => {
+    e.preventDefault();
+    axios({
+      url: "https://61fd0f4cf62e220017ce42d7.mockapi.io/UserTable",
+      method: "POST",
+      data: {
+        fnm: userData.fnm,
+        lnm: userData.lnm,
+        age: userData.age,
+        gender: userData.gen,
+        hobby: userData.hobby,
+      },
     })
-    const handlechange = (e) => {
-        let { name, value } = e.target;
-        if (name === "hobby") {
-            const hobby = { ...userData.hobby };
-            hobby[value] = e.target.checked;
-            setData((prev) => ({ ...prev, hobby }));
-        }
-        else
-            setData((prev) => ({ ...prev, [name]: value }));
-    }
-    console.log(userData);
-    const handleUpload = (e) => {
-        e.preventDefault();
-        axios({
-            url: "https://61fd0f4cf62e220017ce42d7.mockapi.io/UserTable",
-            method: 'POST',
-            data: { 'fnm': userData.fnm, 'lnm': userData.lnm, 'age': userData.age, 'gender': userData.gen, 'hobby': userData.hobby },
-        })
-            .then((res) => {
-                alert("Success!!!");
-                window.history.pushState('data', 'title', '/read');
-                <Route path="/read"><ReadForm /></Route>
-                window.location.reload();
-            })
-            .catch((error) => alert(error));
-    }
-    return (
-        <div>
-            <form onSubmit={e => handleUpload(e)}>
-                <label>First name:
-                    <input type="text" name="fnm" placeholder='First name' onChange={(e) => handlechange(e)} />
-                </label>
-                <label>Last name:
-                    <input type="text" name="lnm" placeholder='Last name' onChange={(e) => handlechange(e)} />
-                </label>
-                <label>Age:
-                    <input type="numer" name="age" placeholder='Age' onChange={(e) => handlechange(e)} />
-                </label>
-                <label>Gender:
-                    <label><input type="radio" name="gen" value="male" onChange={(e) => handlechange(e)} />Male</label>
-                    <label><input type="radio" name="gen" value="female" onChange={(e) => handlechange(e)} />Female</label>
-                </label>
-                <label className='hobby'>
-                    Hobbies:
-                    <label><input type="checkbox" name="hobby" value="Music" onChange={(e) => handlechange(e)} />Music</label>
-                    <label><input type="checkbox" name="hobby" value="Cricket" onChange={(e) => handlechange(e)} />Cricket</label>
-                    <label><input type="checkbox" name="hobby" value="Football" onChange={(e) => handlechange(e)} />Football</label>
-                    <label><input type="checkbox" name="hobby" value="Movies" onChange={(e) => handlechange(e)} />Movies</label>
-                    <label><input type="checkbox" name="hobby" value="Games" onChange={(e) => handlechange(e)} />Games</label>
-                </label>
-                <input type="submit" value="submit" />
-            </form>
-        </div>
-    );
+      .then((res) => {
+        alert("Success!!!");
+        window.history.pushState("data", "title", "/read");
+        <Route path="/read">
+          <ReadForm />
+        </Route>;
+        window.location.reload();
+      })
+      .catch((error) => alert(error));
+  };
+  return (
+    <div>
+      <form onSubmit={(e) => handleUpload(e)}>
+        <label>
+          First name:
+          <input
+            type="text"
+            name="fnm"
+            placeholder="First name"
+            onChange={(e) => handlechange(e)}
+          />
+        </label>
+        <label>
+          Last name:
+          <input
+            type="text"
+            name="lnm"
+            placeholder="Last name"
+            onChange={(e) => handlechange(e)}
+          />
+        </label>
+        <label>
+          Age:
+          <input
+            type="numer"
+            name="age"
+            placeholder="Age"
+            onChange={(e) => handlechange(e)}
+          />
+        </label>
+        <label>
+          Gender:
+          <label>
+            <input
+              type="radio"
+              name="gen"
+              value="male"
+              onChange={(e) => handlechange(e)}
+            />
+            Male
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="gen"
+              value="female"
+              onChange={(e) => handlechange(e)}
+            />
+            Female
+          </label>
+        </label>
+        <label className="hobby">
+          Hobbies:
+          <label>
+            <input
+              type="checkbox"
+              name="hobby"
+              value="Music"
+              onChange={(e) => handlechange(e)}
+            />
+            Music
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="hobby"
+              value="Cricket"
+              onChange={(e) => handlechange(e)}
+            />
+            Cricket
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="hobby"
+              value="Football"
+              onChange={(e) => handlechange(e)}
+            />
+            Football
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="hobby"
+              value="Movies"
+              onChange={(e) => handlechange(e)}
+            />
+            Movies
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="hobby"
+              value="Games"
+              onChange={(e) => handlechange(e)}
+            />
+            Games
+          </label>
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
--- src/Component/read/readform.js
+++ src/Component/read/readform.js
@@ -4,58 +4,74 @@ import { Route } from "react-router-dom";
 import UpdateForm from "../update/editform";
 import "./readform.css";
 export default function ReadForm() {
-    const [userData, setData] = useState([]);
-    useEffect(() => {
-        axios.get("https://61fd0f4cf62e220017ce42d7.mockapi.io/UserTable").then((resp) => {
-            setData(resp.data);
-        });
-    }, []);
-    const deleteData = (id) => {
-        axios.delete(`https://61fd0f4cf62e220017ce42d7.mockapi.io/UserTable/${id}`).then((resp) => {
-            let data1 = userData;
-            let index = data1.findIndex((element) => element.id === resp.data.id);
-            data1.splice(index, 1);
-            setData(data1);
-            window.location.reload();
-        })
-    }
-    const updateData = (element, array) => {
-        window.history.pushState('data', 'title', `/edit/${element.id}/${element.fnm}/${element.lnm}/${element.age}/${element.gender}/${array}`);
-        <Route path='/edit/:id/:fnm/:lnm/:age/:gen/:hobby'><UpdateForm /></Route >
+  const [userData, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://61fd0f4cf62e220017ce42d7.mockapi.io/UserTable")
+      .then((resp) => {
+        setData(resp.data);
+      });
+  }, []);
+  const deleteData = (id) => {
+    axios
+      .delete(`https://61fd0f4cf62e220017ce42d7.mockapi.io/UserTable/${id}`)
+      .then((resp) => {
+        let data1 = userData;
+        let index = data1.findIndex((element) => element.id === resp.data.id);
+        data1.splice(index, 1);
+        setData(data1);
         window.location.reload();
-    }
-    const display = userData.map((element) => {
-        let hobbyArray = Object.values(element.hobby);
-        return (
-            <tr>
-                <td>{element.id}</td>
-                <td>{element.fnm}</td>
-                <td>{element.lnm}</td>
-                <td>{element.age}</td>
-                <td>{element.gender}</td>
-                <td>{(hobbyArray[0] ? "Music " : " ") + (hobbyArray[1] ? "Cricket " : " ") + (hobbyArray[2] ? "Football " : " ") + (hobbyArray[3] ? "Movies " : " ") + (hobbyArray[4] ? "Game " : " ")}</td>
-                <td>
-                    <button onClick={(e) => updateData(element, hobbyArray)}> ✎ </button>
-                    <button onClick={(e) => deleteData(element.id)}> ⚔ </button>
-                </td>
-            </tr>
-        )
-    });
-
+      });
+  };
+  const updateData = (element, array) => {
+    window.history.pushState(
+      "data",
+      "title",
+      `/edit/${element.id}/${element.fnm}/${element.lnm}/${element.age}/${element.gender}/${array}`
+    );
+    <Route path="/edit/:id/:fnm/:lnm/:age/:gen/:hobby">
+      <UpdateForm />
+    </Route>;
+    window.location.reload();
+  };
+  const display = userData.map((element) => {
+    let hobbyArray = Object.values(element.hobby);
     return (
-        <div className="apos">
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                    <th>Hobbies</th>
-                    <th>Options</th>
-                </tr>
-                {display}
-            </table>
-        </div>
-    )
+      <tr>
+        <td>{element.id}</td>
+        <td>{element.fnm}</td>
+        <td>{element.lnm}</td>
+        <td>{element.age}</td>
+        <td>{element.gender}</td>
+        <td>
+          {(hobbyArray[0] ? "Music " : " ") +
+            (hobbyArray[1] ? "Cricket " : " ") +
+            (hobbyArray[2] ? "Football " : " ") +
+            (hobbyArray[3] ? "Movies " : " ") +
+            (hobbyArray[4] ? "Game " : " ")}
+        </td>
+        <td>
+          <button onClick={(e) => updateData(element, hobbyArray)}> ✎ </button>
+          <button onClick={(e) => deleteData(element.id)}> ⚔ </button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="apos">
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+          <th>Gender</th>
+          <th>Hobbies</th>
+          <th>Options</th>
+        </tr>
+        {display}
+      </table>
+    </div>
+  );
 }
